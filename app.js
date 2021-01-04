f38a71288a2416856321aa53d37dcd5583ef5a8e
Hamsa Elmi <73555504+hamsame@users.noreply.github.com>
2021-01-04 17:26:34 +0000

    Update: Solved case sensitivity issue
    I dynamically changed the capitalisation of the user's input to suit the API's requirements. Now regardless of the user's capitalisation the API will now get the requested information. I also capitalised the country name in the small info box that comes from the API.

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ document.querySelector(".get-data").addEventListener("click", getStats);
 const stats = document.querySelector(".stats");
 
 async function getStats(e) {
-  const country = document.querySelector("#country").value;
+  let country = document.querySelector("#country").value;
+  country = country
+    .toLowerCase()
+    .split(" ")
+    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(" ");
+
   const response = await fetch(
     `https://covid-api.mmediagroup.fr/v1/cases?country=${country}`
   );
+
   const data = await response.json();
   let output = "";
   output = `<div class="data">
-  <h4>${country}</h4>
+  <h4>${country.toUpperCase()}</h4>
   <p>Total Confirmed Cases: ${data.All.confirmed}</p>
   <p>Total Recovered: ${data.All.recovered}</p>
   <p style>Total Deaths: </span>${data.All.deaths}</p>
