7a145a66980f828df0d88bad9942884e34d62f7b
Hamsa Elmi <73555504+hamsame@users.noreply.github.com>
2021-01-16 12:58:24 +0000

    Update: changed fetch() to ajax
    I replaced the fetch method with AJAX to make it functional on mobile phones and computers. I also made the delete button work, however it only works when only one table is outputted.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+// event listener
 document.querySelector(".get-data").addEventListener("click", getStats);
-const stats = document.querySelector(".stats");
 
+// function that gets Stats
 async function getStats(e) {
   let country = document.querySelector("#country").value;
   country = country
@@ -9,20 +10,38 @@ async function getStats(e) {
     .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
     .join(" ");
 
-  const response = await fetch(
-    `https://covid-api.mmediagroup.fr/v1/cases?country=${country}`
+  // get data requested from API using AJAX
+  const data = new XMLHttpRequest();
+  data.open(
+    "GET",
+    `https://covid-api.mmediagroup.fr/v1/cases?country=${country}`,
+    true
   );
 
-  const data = await response.json();
   let output = "";
-  output = `<div class="data">
-  <h4>${country.toUpperCase()}</h4> 
-  <p>Total Confirmed Cases: ${data.All.confirmed.toLocaleString()}</p>
-  <p>Total Recovered: ${data.All.recovered.toLocaleString()}</p>
-  <p style>Total Deaths: </span>${data.All.deaths.toLocaleString()}</p>
-  <p>Life Expectancy: ${data.All.life_expectancy.toLocaleString()}
-  <p>Continent: ${data.All.continent.toLocaleString()}</p>
-  Delete: <i id="bin" class="fas fa-trash-alt"></i></div>`;
-  document.querySelector(".stats").innerHTML += output;
+  data.onload = async function () {
+    if (data.status == 200) {
+      const data = await JSON.parse(this.responseText);
+      output = `<div class="data">
+        <h4>${country.toUpperCase()}</h4>
+        <p>Total Confirmed Cases: ${data.All.confirmed.toLocaleString()}</p>
+        <p>Total Recovered: ${data.All.recovered.toLocaleString()}</p>
+        <p style>Total Deaths: </span>${data.All.deaths.toLocaleString()}</p>
+        <p>Life Expectancy: ${data.All.life_expectancy.toLocaleString()}
+        <p>Continent: ${data.All.continent.toLocaleString()}</p>
+        Delete: <i id="bin" class="fas fa-trash-alt"></i></div>`;
+      document.querySelector(".stats").innerHTML += output;
+
+      // delete info table
+      document.querySelector("#bin").addEventListener("click", deleteInfo);
+      function deleteInfo(e) {
+        if (e.target.parentElement.className == "data") {
+          e.target.parentElement.remove();
+        }
+      }
+    }
+  };
+
   e.preventDefault();
+  data.send();
 }
