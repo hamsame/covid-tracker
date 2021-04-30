fa75c3ec69d153193f39dd7599f98f3ae2b0741e
Hamsa Elmi <73555504+hamsame@users.noreply.github.com>
2021-04-30 14:41:49 +0100

    Solved bug
    Solved bug where some my script would not add info for a country because the API had not had the continent of the country named.

--- app.js
+++ app.js
@@ -1,74 +1,78 @@
 // Gets names of countries and appends it to selection
-document.addEventListener("DOMContentLoaded", (e) => {
-  const request = new XMLHttpRequest();
-  request.open("GET", "https://covid-api.mmediagroup.fr/v1/cases", true);
+document.addEventListener('DOMContentLoaded', (e) => {
+  const request = new XMLHttpRequest()
+  request.open('GET', 'https://covid-api.mmediagroup.fr/v1/cases', true)
 
   request.onload = () => {
     if (request.status == 200) {
-      const res = JSON.parse(request.responseText);
+      const res = JSON.parse(request.responseText)
       for (country in res) {
-        var option = document.createElement("option");
-        option.className = "option";
-        option.value = `${country}`;
-        var optionValue = document.createTextNode(country);
-        option.appendChild(optionValue);
-        document.querySelector("#countryy").appendChild(option);
+        var option = document.createElement('option')
+        option.className = 'option'
+        option.value = `${country}`
+        var optionValue = document.createTextNode(country)
+        option.appendChild(optionValue)
+        document.querySelector('#countryy').appendChild(option)
       }
     }
-  };
-  request.send();
-  e.preventDefault();
-});
+  }
+  request.send()
+  e.preventDefault()
+})
 
 // event listener for function that gets stats
-document.querySelector(".get-data").addEventListener("click", getStats);
+document.querySelector('.get-data').addEventListener('click', getStats)
 async function getStats(e) {
-  let selectedCountry = document.querySelector("#countryy");
-  let country = selectedCountry.value;
+  let selectedCountry = document.querySelector('#countryy')
+  let country = selectedCountry.value
   // get data requested from API using AJAX
-  const data = new XMLHttpRequest();
+  const data = new XMLHttpRequest()
   data.open(
-    "GET",
+    'GET',
     `https://covid-api.mmediagroup.fr/v1/cases?country=${country}`,
     true
-  );
+  )
 
-  let output = "";
+  let output = ''
   data.onload = async function () {
     if (data.status == 200) {
-      const data = await JSON.parse(this.responseText);
-      if (country !== "Global") {
+      const data = await JSON.parse(this.responseText)
+      let continent = ''
+      if (data.All.continent == undefined || null) {
+        continent = `<p>Continent: ${data.All.continent}</p>`
+      }
+      if (country !== 'Global') {
         output = `<div class="data">
         <h4>${data.All.country}</h4>
-        <p>Total Confirmed Cases: ${data.All.confirmed.toLocaleString("en")}</p>
-        <p>Total Recovered: ${data.All.recovered.toLocaleString("en")}</p>
+        <p>Total Confirmed Cases: ${data.All.confirmed.toLocaleString('en')}</p>
+        <p>Total Recovered: ${data.All.recovered.toLocaleString('en')}</p>
         <p style>Total Deaths: </span>${data.All.deaths.toLocaleString(
-          "en"
+          'en'
         )}</p>
-        <p>Continent: ${data.All.continent.toLocaleString("en")}</p>
-        Delete: <i id="bin" class="fas fa-trash-alt"></i></div>`;
-        document.querySelector(".stats").innerHTML += output;
+        ${continent}
+        Delete: <i id="bin" class="fas fa-trash-alt"></i></div>`
+        document.querySelector('.stats').innerHTML += output
       } else {
         output = `<div class="data">
         <h4>Global</h4>
-        <p>Total Confirmed Cases: ${data.All.confirmed.toLocaleString("en")}</p>
-        <p>Total Recovered: ${data.All.recovered.toLocaleString("en")}</p>
+        <p>Total Confirmed Cases: ${data.All.confirmed.toLocaleString('en')}</p>
+        <p>Total Recovered: ${data.All.recovered.toLocaleString('en')}</p>
         <p style>Total Deaths: </span>${data.All.deaths.toLocaleString(
-          "en"
+          'en'
         )}</p>
-        Delete: <i id="bin" class="fas fa-trash-alt"></i></div>`;
-        document.querySelector(".stats").innerHTML += output;
+        Delete: <i id="bin" class="fas fa-trash-alt"></i></div>`
+        document.querySelector('.stats').innerHTML += output
       }
 
       // delete info table
-      document.querySelector("#bin").addEventListener("click", deleteInfo);
+      document.querySelector('#bin').addEventListener('click', deleteInfo)
       function deleteInfo(e) {
-        if (e.target.parentElement.className == "data") {
-          e.target.parentElement.remove();
+        if (e.target.parentElement.className == 'data') {
+          e.target.parentElement.remove()
         }
       }
     }
-  };
-  e.preventDefault();
-  data.send();
+  }
+  e.preventDefault()
+  data.send()
 }
